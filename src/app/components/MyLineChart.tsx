36f3e18bc02e5982288261bcebaac7bdde829403
Ko Nishiyama <nishiyama.ko.0420@gmail.com>
2024-04-25 21:28:32 +0900

    first commit

---
+++ src/app/components/MyLineChart.tsx
@@ -0,0 +1,52 @@
+// components/MyLineChart.tsx
+"use client";
+
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  Tooltip,
+  PointElement,
+  LineElement,
+} from "chart.js";
+import { Line } from "react-chartjs-2";
+
+// Register ChartJS components using ChartJS.register
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Tooltip
+);
+
+const MyLineChart = () => {
+  return (
+    <div>
+      <Line
+        data={{
+          labels: [
+            "2023-01",
+            "2023-02",
+            "2023-03",
+            "2023-04",
+            "2023-05",
+            "2023-06",
+            "2023-07",
+          ],
+          datasets: [
+            {
+              data: [100, 120, 115, 134, 168, 132, 200],
+              backgroundColor: "purple",
+            },
+            {
+              data: [150, 10, 45, 54, 68, 32, 100],
+              backgroundColor: "red",
+            },
+          ],
+        }}
+      />
+    </div>
+  );
+};
+export default MyLineChart;
